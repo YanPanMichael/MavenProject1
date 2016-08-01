c1d3eec2152b3937af63218f082983857b3e1b7a
Michael <yanpancsu@icloud.com>
2016-08-02 00:21:56 +0800

    Total commit

---
+++ src/main/java/com/maven/spark/exec1/SparkExercise1/App.java
@@ -0,0 +1,13 @@
+package com.maven.spark.exec1.SparkExercise1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
